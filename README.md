658c56b45f24f76f09421b06034a98873bcd529a
r.fathi <r.fathi@salespower.ma>
2024-06-08 13:26:21 +0100

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# react-native-zebra-ble-printer
+
+A React Native Library to support zebra bluetooth printer
+
+## Installation
+
+```sh
+npm install react-native-zebra-ble-printer
+```
+
+## Usage
+
+```js
+import { multiply } from 'react-native-zebra-ble-printer';
+
+// ...
+
+const result = await multiply(3, 7);
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
+
+---
+
+Made with [create-react-native-library](https://github.com/callstack/react-native-builder-bob)
---
+++ android/src/main/java/com/zebrableprinter/ZebraBlePrinterModule.java
@@ -0,0 +1,32 @@
+package com.zebrableprinter;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = ZebraBlePrinterModule.NAME)
+public class ZebraBlePrinterModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "ZebraBlePrinter";
+
+  public ZebraBlePrinterModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  public void multiply(double a, double b, Promise promise) {
+    promise.resolve(a * b);
+  }
+}
